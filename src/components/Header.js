cbaad265811456bfd717baa52d8719d9ddca93f0
Vatibor <vatibor2@gmail.com>
2021-09-06 22:06:28 +0200

    create basket page and some logic as add, remove with redux global store

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,22 @@
 import Image from "next/image";
 import { MenuIcon, SearchIcon, ShoppingCartIcon } from "@heroicons/react/outline";
 import { signIn, signOut, useSession } from "next-auth/client";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { selectItems } from "../slices/basketSlice";
 
 function Header() {
     const [session] = useSession();
-
+    const router = useRouter();
+    const items = useSelector(selectItems);
 
     return (
         <header>
             {/* Top nav */}
             <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
                 <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
-                    <Image 
+                    <Image
+                        onClick={() => router.push("/")} 
                         src="/amazon_PNG11.png"
                         width={150}
                         height={40}
@@ -40,8 +45,8 @@ function Header() {
                         <p className="font-extrabold md:text-sm">& Orders</p>
                     </div>
 
-                    <div className="link relative flex items-center">
-                        <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">4</span>
+                    <div onClick={() => router.push("/checkout")} className="link relative flex items-center">
+                        <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">{items.length}</span>
                         <ShoppingCartIcon className="h-10" />
                         <p className="font-extrabold md:text-sm hidden md:inline mt-2">Basket</p>
                     </div>
